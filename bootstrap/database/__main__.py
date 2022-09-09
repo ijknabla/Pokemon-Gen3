3bca94aa3a7d76b26c10e52c6a26c22f148d7634
ijknabla <ijknabla@gmail.com>
2022-09-09 23:20:55 +0900

    Implement get_pokemon_name_jp

--- bootstrap/database/__main__.py
+++ bootstrap/database/__main__.py
@@ -1,14 +1,20 @@
 import lxml.html
-import json
 import re
 import requests
-import sys
 from typing import Iterator, Tuple
 
 
-
 def main() -> None:
-    json.dump(list(get_pokemon_name_en()), sys.stdout)
+    names_en = dict(get_pokemon_name_en())
+    names_jp = dict(get_pokemon_name_jp())
+
+    keys = sorted(names_en.keys() | names_jp.keys())
+    for key in keys:
+        name_en = names_en[key]
+        print(key, "en", name_en)
+    for key in keys:
+        name_jp = names_jp[key]
+        print(key, "jp", name_jp)
 
 
 def get_pokemon_name_en() -> Iterator[Tuple[int, str]]:
@@ -23,6 +29,26 @@ def get_pokemon_name_en() -> Iterator[Tuple[int, str]]:
                     int(re.search(r"#(\d+)", tr[0].text).group(1)),
                     re.search(r"\S+", tr[2][0].text).group(0),
                 )
+
+
+def get_pokemon_name_jp() -> Iterator[Tuple[int, str]]:
+    uri = "https://wiki.xn--rckteqa2e.com/wiki/ポケモン一覧_(第五世代まで)"
+    with requests.get(uri) as response:
+        element = lxml.html.fromstring(response.text)
+        for tr in element.xpath(
+            "//tr[../../../table[@class='bluetable c sortable']]"
+        ):
+            try:
+                i = int(tr[0].text.strip())
+                if 386 < i:
+                    break
+            except (AttributeError, ValueError):
+                continue
+            yield i, tr[1][0].text.strip()
+
+        for tr in element.xpath("//table"):
+            print(tr.attrib["class"])
+        yield from []
 
 
 if __name__ == "__main__":
