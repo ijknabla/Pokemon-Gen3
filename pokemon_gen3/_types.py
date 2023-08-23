81eac59d38935f92729c582658f84582e2c974b3
ijknabla <ijknabla@gmail.com>
2023-08-23 11:38:29 +0900

    Fix type-hint for python 3.7.x

--- pokemon_gen3/_types.py
+++ pokemon_gen3/_types.py
@@ -5,12 +5,15 @@ __all__ = (
     "StatID",
 )
 
-from typing import (
-    Literal,
-    NewType,
-)
+from typing import NewType, TYPE_CHECKING
 
-Enhancement = Literal[-1, 0, 1]
+if TYPE_CHECKING:
+    from typing_extensions import Literal
+
+if TYPE_CHECKING:
+    Enhancement = Literal[-1, 0, 1]
+else:
+    Enhancement = int
 
 NatureID = NewType("NatureID", int)
 PokemonID = NewType("PokemonID", int)
