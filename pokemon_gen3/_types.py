ddfffaf541784076bbc10372ce2f816f317b3f17
ijknabla <ijknabla@gmail.com>
2023-08-23 11:13:05 +0900

    Implement Language enum

--- pokemon_gen3/_types.py
+++ pokemon_gen3/_types.py
@@ -1,10 +1,12 @@
 __all__ = (
     "Enhancement",
+    "Language",
     "NatureID",
     "PokemonID",
     "StatID",
 )
 
+import enum
 from typing import TYPE_CHECKING, NewType
 
 if TYPE_CHECKING:
@@ -14,6 +16,14 @@ if TYPE_CHECKING:
     Enhancement = Literal[-1, 0, 1]
 else:
     Enhancement = int
+
+
+class Language(enum.Enum):
+    ja, en = enum.auto(), enum.auto()
+
+    Japanese = 日本語 = ja
+    English = en
+
 
 NatureID = NewType("NatureID", int)
 PokemonID = NewType("PokemonID", int)
