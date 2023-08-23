3600d1d6e3f352b1194e490b83f763cc771cba50
ijknabla <ijknabla@gmail.com>
2023-08-23 12:54:18 +0900

    Remove old table

--- bootstrap/database/stat.sql
+++ bootstrap/database/stat.sql
@@ -1,21 +1,4 @@
 
-CREATE TABLE stats
-(
-    id INTEGER PRIMARY KEY,
-    name_en TEXT UNIQUE NOT NULL,
-    name_jp TEXT UNIQUE NOT NULL
-);
-
-INSERT INTO stats
-    (id, name_en  , name_jp  )
-VALUES
-    ( 0, 'HP'     , 'HP'     ),
-    ( 1, 'Attack' , 'こうげき'),
-    ( 2, 'Defence', 'ぼうぎょ'),
-    ( 3, 'Sp. Atk', 'とくこう'),
-    ( 4, 'Sp. Def', 'とくぼう'),
-    ( 5, 'Speed'  , 'すばやさ');
-
 CREATE TABLE stat_names
 (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
--- pokemon_gen3/database/stat.py
+++ pokemon_gen3/database/stat.py
@@ -1,4 +1,3 @@
-import functools
 from typing import cast
 
 from .._types import Language, StatID
@@ -14,36 +13,6 @@ FROM stat_names
 WHERE stat_id=:id AND language_id=:language_name
         """,
         {"id": id, "language_name": language.name},
-    )
-    (value,) = cursor.fetchone()
-    return cast(str, value)
-
-
-@functools.lru_cache()
-def name_en_by_id(id_: StatID) -> str:
-    cursor = get_connection().cursor()
-    cursor.execute(
-        """
-SELECT name_en FROM stats WHERE id=:id
-        """,
-        {
-            "id": id_,
-        },
-    )
-    (value,) = cursor.fetchone()
-    return cast(str, value)
-
-
-@functools.lru_cache()
-def name_jp_by_id(id_: StatID) -> str:
-    cursor = get_connection().cursor()
-    cursor.execute(
-        """
-SELECT name_jp FROM stats WHERE id=:id
-        """,
-        {
-            "id": id_,
-        },
     )
     (value,) = cursor.fetchone()
     return cast(str, value)
