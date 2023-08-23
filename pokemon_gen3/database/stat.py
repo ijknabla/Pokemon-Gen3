42362612a9adac7e4aeda0f0c9391e4be143ab76
ijknabla <ijknabla@gmail.com>
2023-08-23 12:47:57 +0900

    Implement name_by_id

--- pokemon_gen3/database/stat.py
+++ pokemon_gen3/database/stat.py
@@ -1,8 +1,22 @@
 import functools
 from typing import cast
 
-from .._types import StatID
+from .._types import Language, StatID
 from ._connection import get_connection
+
+
+def name_by_id(id: StatID, language: Language) -> str:
+    cursor = get_connection().cursor()
+    cursor.execute(
+        """
+SELECT name
+FROM stat_names
+WHERE stat_id=:id AND language_id=:language_name
+        """,
+        {"id": id, "language_name": language.name},
+    )
+    (value,) = cursor.fetchone()
+    return cast(str, value)
 
 
 @functools.lru_cache()
