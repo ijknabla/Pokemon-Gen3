79abaa36b087104814efe94c5f31020f9b22cae1
ijknabla <ijknabla@gmail.com>
2023-08-24 11:14:27 +0900

    Implement pokemon_gen3.database.pokemon.{id_by_name, name_by_id}

--- pokemon_gen3/database/pokemon.py
+++ pokemon_gen3/database/pokemon.py
@@ -1,6 +1,6 @@
 from typing import Iterator, Tuple, cast
 
-from .._types import PokemonID
+from .._types import Language, PokemonID
 from ._connection import get_connection
 
 
@@ -15,6 +15,34 @@ ORDER BY id
     )
     for (id_,) in cursor.fetchall():
         yield id_
+
+
+def id_by_name(name: str) -> PokemonID:
+    cursor = get_connection().cursor()
+    cursor.execute(
+        """
+SELECT pokemon_id
+FROM pokemon_names
+WHERE name=:name
+        """,
+        {"name": name},
+    )
+    (value,) = cursor.fetchone()
+    return cast(PokemonID, value)
+
+
+def name_by_id(id: PokemonID, language: Language) -> str:
+    cursor = get_connection().cursor()
+    cursor.execute(
+        """
+SELECT name
+FROM pokemon_names
+WHERE pokemon_id=:id AND language_id=:language_name
+        """,
+        {"id": id, "language_name": language.name},
+    )
+    (value,) = cursor.fetchone()
+    return cast(str, value)
 
 
 def id_by_name_jp(
